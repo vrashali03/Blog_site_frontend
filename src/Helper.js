499623320336953d5cbff158ad8fe4e154cff135
Vrashali Naitam <vrushali2214@gmail.com>
2024-06-11 00:52:53 +0530

    Added new backend site

--- src/Helper.js
+++ src/Helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "https://blog-website-backend-xq7j.onrender.com";
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://blog-site-backend-rrij.onrender.com";
